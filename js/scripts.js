19d84a18b20471a8db1416f42a06bf6cadb6b05f
Brian-Muchera <mucherabrian2@gmail.com>
2020-08-09 16:11:59 +0300

    added some functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,12 @@
-$(document).ready(function() {
+$(document).ready(function () {
     $(".text").hide();
-    $(".bol").click(function() {
-        $(".text").slideToggle("fast");
+    $(".bol").click(function () {
+        $(".text").fadeToggle("slow");
     });
     $(".jaza").hide();
-    $(".pic").click(function() {
+    $(".pic").click(function () {
         $(".jaza").fadeToggle("slow");
-});
+    });
 });
 function validate() {
     var name = document.getElementById("name").value;
@@ -30,4 +30,19 @@ function validate() {
         alert("I have received  your message " + name + ", " + " Thanks for the feedback!!!!");
     };
 
+}
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("slides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1 }
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 2000);
 }
